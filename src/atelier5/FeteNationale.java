c0d10290c43565c1f78716a2062b3ae35445bf0d
langueetinformatique2021 <langueetinformatique2021@gmail.com>
2023-11-16 19:01:56 +0100

    modif

--- src/atelier5/FeteNationale.java
+++ src/atelier5/FeteNationale.java
@@ -5,7 +5,7 @@ public class FeteNationale {
 	public static void main(String[] args) {
 		
 		// Instanciation de deux objets de type date
-		Date today = new Date(9, 11, 2023); 
+		Date today = new Date(16, 11, 2023); 
 		//Date dfin = new Date(10, 11, 2023);
 		Date dfin = new Date(14, 7, 2024);
 		int njours = 0;
