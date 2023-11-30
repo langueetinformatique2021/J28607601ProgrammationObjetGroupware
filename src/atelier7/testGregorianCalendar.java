1a5b08d150fcd67747643b28c151f9536e667e5e
isha <isha@fedora>
2023-11-30 11:28:17 +0100

    Exercice 1 Question 2

---
+++ src/atelier7/testGregorianCalendar.java
@@ -0,0 +1,15 @@
+package atelier7;
+
+
+import java.util.*;
+
+public class testGregorianCalendar {
+
+	public static void main(String[] args) {
+		GregorianCalendar gc = new GregorianCalendar();
+		Date d = gc.getTime();
+		System.out.println(d.toString());
+
+	}
+
+}
